c53f0a4264a348362a427092f86a2b345049f974
Diego <dsimog01@estudiantes.unileon.es>
2023-08-23 15:51:13 +0200

    Allow user config for RPCs

--- op-geth/entrypoint.sh
+++ op-geth/entrypoint.sh
@@ -2,8 +2,9 @@
 
 # Configuration defined in https://community.optimism.io/docs/developers/bedrock/node-operator-guide/#configuring-op-geth
 
-echo "[INFO - entrypoint] Initializing Geth from genesis"
-geth --datadir=/data init /config/genesis.json
+# Genesis is hardcoded for mainnet and goerli, not needed here
+#echo "[INFO - entrypoint] Initializing Geth from genesis"
+#geth --datadir=/data init /config/genesis.json
 
 # Tx pool gossip is disabled as it is not supported yet
 # Max peers set to 0 to disable peer discovery (will be enabled in the future for snap sync)
@@ -11,9 +12,9 @@ geth --datadir=/data init /config/genesis.json
 
 echo "[INFO - entrypoint] Starting Geth"
 exec geth --datadir /data \
-  --rollup.historicalrpc http://l2geth.dappnode:8545 \
-  --rollup.sequencerhttp https://bedrock-beta-1-sequencer.optimism.io \
-  --rollup.disabletxpoolgossip true \
+  --rollup.historicalrpc $L2_NODE_RPC_URL \
+  --rollup.sequencerhttp $SEQUENCER_HTTP_URL \
+  --rollup.disabletxpoolgossip \
   --ws \
   --ws.port 8546 \
   --ws.addr 0.0.0.0 \
