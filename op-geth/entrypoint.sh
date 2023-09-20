a08b31881a65d56413b64c5c660cd23f07c3f8d5
dappnodedev <144998261+dappnodedev@users.noreply.github.com>
2023-09-20 15:30:08 +0200

    Enable historical RPC as global env (#3)
    Co-authored-by: Diego <dsimog01@estudiantes.unileon.es>

--- op-geth/entrypoint.sh
+++ op-geth/entrypoint.sh
@@ -8,7 +8,14 @@ DATA_DIR=/data
 # Max peers set to 0 to disable peer discovery (will be enabled in the future for snap sync)
 # TODO: Should we add --http.api and --ws.api flags?
 
-if [ "$ENABLE_HISTORICAL_RPC" = "true" ]; then
+if [ "$_DAPPNODE_GLOBAL_OP_ENABLE_HISTORICAL_RPC" = "true" ]; then
+
+  if [ -z "$HISTORICAL_RPC_URL" ]; then
+    echo "[ERROR - entrypoint] ENABLE_HISTORICAL_RPC is set to true but HISTORICAL_RPC_URL is not defined"
+    sleep 60
+    exit 1
+  fi
+
   echo "[INFO - entrypoint] Enabling historical RPC"
   EXTRA_FLAGS="--rollup.historicalrpc $HISTORICAL_RPC_URL --gcmode archive"
 else
@@ -20,23 +27,14 @@ fi
 if [ "$(ls -A $DATA_DIR)" ]; then
   echo "[INFO - entrypoint] Database already exists, skipping initialization"
 else
-  echo "[INFO - entrypoint] $DATA_DIR is empty, initializing geth..."
-
-  # If GENESIS_MODE is set to GenesisFile (non case sensitive), then geth will be initialized from it
-  if [ "$GENESIS_MODE" = "GenesisFile" ]; then
-    echo "[INFO - entrypoint] Initializing geth from genesis file"
-    geth --datadir=$DATA_DIR init /config/genesis.json
-  else
-    echo "[INFO - entrypoint] Initializing geth from preloaded data"
-    echo "[INFO - entrypoint] Downloading preloaded data from $PRELOADED_DATA_URL. This can take hours..."
-    mkdir -p $DATA_DIR
-    wget -O /preloaded-mainnet-data/mainnet-bedrock.tar.zst https://datadirs.optimism.io/mainnet-bedrock.tar.zst
-    echo "[INFO - entrypoint] Decompressing preloaded data. This can take a while..."
-    zstd -d --stdout /preloaded-mainnet-data/mainnet-bedrock.tar.zst | tar xvf - -C $DATA_DIR
-    rm -rf /preloaded-mainnet-data
-    EXTRA_FLAGS="--datadir.ancient $DATA_DIR/geth/chaindata/ancient"
-  fi
-
+  echo "[INFO - entrypoint] $DATA_DIR is empty, initializing geth from preloaded data"
+  echo "[INFO - entrypoint] Downloading preloaded data from $PRELOADED_DATA_URL. This can take hours..."
+  mkdir -p $DATA_DIR
+  wget -O /preloaded-mainnet-data/mainnet-bedrock.tar.zst https://datadirs.optimism.io/mainnet-bedrock.tar.zst
+  echo "[INFO - entrypoint] Decompressing preloaded data. This can take a while..."
+  zstd -d --stdout /preloaded-mainnet-data/mainnet-bedrock.tar.zst | tar xvf - -C $DATA_DIR
+  rm -rf /preloaded-mainnet-data
+  EXTRA_FLAGS="--datadir.ancient $DATA_DIR/geth/chaindata/ancient"
 fi
 
 echo "[INFO - entrypoint] Starting Geth"
