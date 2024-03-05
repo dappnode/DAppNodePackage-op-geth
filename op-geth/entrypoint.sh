e38470ed4ab492c03e4536c6c69420cba6460904
Voss <voss@visnovalabs.io>
2024-03-05 01:04:44 -0600

    add WS and HTTP API flags

--- op-geth/entrypoint.sh
+++ op-geth/entrypoint.sh
@@ -7,7 +7,6 @@ PRELOADED_DATA_FILE=/mainnet-bedrock.tar.zst
 
 # Tx pool gossip is disabled as it is not supported yet
 # Max peers set to 0 to disable peer discovery (will be enabled in the future for snap sync)
-# TODO: Should we add --http.api and --ws.api flags?
 
 if [ "$_DAPPNODE_GLOBAL_OP_ENABLE_HISTORICAL_RPC" = "true" ]; then
 
@@ -66,11 +65,13 @@ exec geth --datadir $DATA_DIR \
   --ws.port 8546 \
   --ws.addr 0.0.0.0 \
   --ws.origins "*" \
+  --ws.api net,web3,txpool,eth,debug,engine \
   --http \
   --http.port 8545 \
   --http.addr 0.0.0.0 \
   --http.vhosts "*" \
   --http.corsdomain "*" \
+  --http.api web3,net,eth,engine,txpool,debug \
   --authrpc.addr 0.0.0.0 \
   --authrpc.port 8551 \
   --authrpc.vhosts "*" \
